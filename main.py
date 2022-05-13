858553a0842ba7490d08a872a67e9858b08ab522
ProgRiver <barkas.ssr@mail.ru>
2022-05-13 16:12:35 +0300

    new server

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 # webserver settings
 WEBAPP_HOST = "0.0.0.0"
-WEBAPP_PORT = os.getenv("PORT", default=8000)
+WEBAPP_PORT = os.getenv("PORT", default=5000)
 
 logging.basicConfig(level=logging.INFO)
 
